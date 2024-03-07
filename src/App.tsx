2ac2109ea69d88357b5b82eca1f0174b4bf88084
Haneul-Song <haneulisstudying@gmail.com>
2024-03-06 20:38:16 -0700

    Completed Mission 9

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,61 @@
-import "./App.css";
-import logo from "./logo.svg";
+import React from 'react';
+import './App.css';
+import teamsData from './teams.json';
+
+interface TeamProps {
+  tid: number;
+  cid: number;
+  did: number;
+  school: string;
+  name: string;
+  abbrev: string;
+  pop: number;
+  city: string;
+  state: string;
+  latitude: number;
+  longitude: number;
+}
+
+class Team extends React.Component<TeamProps> {
+  render() {
+    const { school, name, city, state } = this.props;
+
+    return (
+      <div>
+        <h2>{school}</h2>
+        <p>Mascot: {name}</p>
+        <p>
+          Location: {city}, {state}
+        </p>
+      </div>
+    );
+  }
+}
+
+const TeamList = () => {
+  return (
+    <div>
+      {teamsData.teams.map((team, index) => (
+        <Team key={index} {...team} />
+      ))}
+    </div>
+  );
+};
+
+function Heading() {
+  return (
+    <header>
+      <h1>Wel come to College Basketball Teams</h1>
+      <p>Explore teams participating in NCAA Basketball.</p>
+    </header>
+  );
+}
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">
+      <Heading />
+      <TeamList />
     </div>
   );
 }
